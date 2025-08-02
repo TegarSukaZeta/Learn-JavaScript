597aa59ae459896ff64e025720c856fb1a986153
TegarSukaZeta <nataliasuparmi27@gmail.com>
2025-08-02 22:16:52 +0700

    JavaScript Beginner Day 3

---
+++ (Day 3) JSTypeConversion/index.js
@@ -0,0 +1,29 @@
+//Type Conversion = change the datatype of a value to another
+//                  (strings, numbers, and booleans)
+
+
+//Convert strings to an numbers
+/*
+let age = window.prompt(`How old are you?`);
+
+age = Number(age);
+age += 1;
+
+console.log(`Happy birthday! you are`, age, `years old`);
+*/
+
+
+/////////////////////////////////
+
+let x;
+let y;
+let z;
+
+x = Number(`3.14`); 
+y = String(3.14);
+z = Boolean(``); // if that empty = false, if that not empty = true
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
