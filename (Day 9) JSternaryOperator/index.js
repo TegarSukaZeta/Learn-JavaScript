2f5bbb3081747052bef7604e18fcc166f4a93e56
TegarSukaZeta <nataliasuparmi27@gmail.com>
2025-08-08 20:56:43 +0700

    JavaScript Beginner Day 8 - 9

---
+++ (Day 9) JSternaryOperator/index.js
@@ -0,0 +1,51 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                    help to assign a variable based on a condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
+
+// Example
+/*
+let age = 21;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+*/
+
+
+////////////////////////
+
+/*
+let age = 21; 
+let message;
+
+if(age >= 18) {
+    message = "You're an adult";
+}
+else {
+    message = "You're a minor";
+}
+*/
+
+
+////////////////////////
+
+/*
+let age = 21;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+*/
+
+
+/////////////////////////
+
+/*
+let time = 4;
+let greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+console.log(greeting);
+*/
+
+
+/////////////////////////
+
+let isStudent = true;
+let messages = isStudent ? "You are a student" : "You aren't a student";
+console.log(messages);
+
